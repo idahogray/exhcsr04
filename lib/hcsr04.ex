c8813424dd994f08a2e4cb231d28afd6cec426b7
BuildTools <unconfigured@null.spigotmc.org>
2017-12-01 14:38:38 +0000

    Integrated the elixirale library with interrupts - not working correctly yet

--- lib/hcsr04.ex
+++ lib/hcsr04.ex
@@ -6,9 +6,12 @@ use GenStateMachine, callback_mode: [:state_functions, :state_enter]
 alias ElixirALE.GPIO
 
   def init(_) do
-    {:ok, trigger_pid} = GPIO.start_link(18, :output)
-    {:ok, echo_pid} = GPIO.start_link(17, :input)
+    IO.puts("init")
+    {:ok, trigger_pid} = GPIO.start_link(23, :output)
+    {:ok, echo_pid} = GPIO.start_link(24, :input)
     GPIO.set_int(echo_pid, :both)
+    IO.puts("\tSetting TRIG Low")
+    GPIO.write(trigger_pid, 0)
     {:ok, :idle, [trigger_pid, echo_pid], {:state_timeout, 1000, :make_measurement_timeout}}
   end
   
@@ -20,13 +23,14 @@ alias ElixirALE.GPIO
   end
   
   def idle(:state_timeout, :make_measurement_timeout, [trigger_pid, echo_pid]) do
-    IO.puts("Time to make a measurement")
-    IO.puts("\tSET TRIG=TRUE")
-    {:next_state, :triggering, [trigger_pid, echo_pid], [{:state_timeout, 1000, :trigger_true_timeout}]}
+    IO.puts("\tTime to make a measurement")
+    IO.puts("\tSET TRIG High")
+    GPIO.write(trigger_pid, 1)
+    {:next_state, :triggering, [trigger_pid, echo_pid], [{:state_timeout, 1, :trigger_true_timeout}]}
   end
 
   def idle(:info, {:gpio_interrupt, echo_pin, direction}, [trigger_pid, echo_pid]) do
-    IO.puts("ECHO Pin Interrupt Received: #{inspect(direction)}")
+    IO.puts("\tECHO Pin Interrupt Received: #{inspect(direction)}")
     :keep_state_and_data
   end
 
@@ -36,36 +40,36 @@ alias ElixirALE.GPIO
   end
   
   def triggering(:state_timeout, :trigger_true_timeout, [trigger_pid, echo_pid]) do
-    IO.puts("TRIG is TRUE timeout received")
-    IO.puts("\tSET TRIG=FALSE")
-    {:next_state, :start_measurement, [trigger_pid, echo_pid], {:timeout, 1000, :falling_echo_interrupt}}
+    IO.puts("\tSET TRIG Low")
+    GPIO.write(trigger_pid, 0)
+    {:next_state, :start_measurement, [trigger_pid, echo_pid]}
   end
   
   def start_measurement(:enter, _old_state, [trigger_pid, echo_pid]) do
-   IO.puts("Entered the start_measurement state")
-   IO.puts("Waiting for falling edge interrupt on ECHO")
+    IO.puts("Entered the start_measurement state")
+    IO.puts("\tWaiting for falling edge interrupt on ECHO")
     :keep_state_and_data
   end
   
-  def start_measurement(:timeout, :falling_echo_interrupt, [trigger_pid, echo_pid]) do
-    IO.puts("Falling Edge of Echo Detected")
+  def start_measurement(:info, {:gpio_interrupt, echo_pin, :rising}, [trigger_pid, echo_pid]) do
+    IO.puts("\tRising Edge of Echo Detected")
     start_time = Timex.now()
-    IO.puts("Start Time: #{inspect(start_time)}")
-    {:next_state, :end_measurement, [trigger_pid, echo_pid, start_time], {:timeout, 1000, :rising_echo_interrupt}}
+    IO.puts("\tStart Time: #{inspect(start_time)}")
+    {:next_state, :end_measurement, [trigger_pid, echo_pid, start_time]}
   end
   
   def end_measurement(:enter, _old_state, [trigger_pid, echo_pid, start_time]) do
     IO.puts("Entered the end_measurement state")
-    IO.puts("Waiting for rising edge interrupt on ECHO")
+    IO.puts("\tWaiting for rising edge interrupt on ECHO")
     :keep_state_and_data
   end
 
-  def end_measurement(:timeout, :rising_echo_interrupt, [trigger_pid, echo_pid, start_time]) do
-    IO.puts("Rising Edge of Echo Detected")
+  def end_measurement(:info, {:gpio_interrupt, echo_pin, :falling}, [trigger_pid, echo_pid, start_time]) do
+    IO.puts("\tFalling Edge of Echo Detected")
     end_time = Timex.now()
-    IO.puts("End Time: #{inspect(start_time)}")
+    IO.puts("\tEnd Time: #{inspect(end_time)}")
     echo_duration = Timex.diff(end_time, start_time, :milliseconds) / 1000
-    IO.puts("Echo Duration: #{inspect(echo_duration)}")
+    IO.puts("\tEcho Duration: #{inspect(echo_duration)}")
     distance_cm = echo_duration * 17150
     distance_in = distance_cm * 0.393701
     IO.puts("Distance: #{inspect(distance_cm)} cm")
